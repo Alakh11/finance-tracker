e580af2f32ba097ad89910d036fb099cc693e910
Alakh <alakh1304@gmail.com>
2026-01-29 14:56:17 +0530

    update

--- server/database.py
+++ server/database.py
@@ -2,21 +2,24 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
-def get_db_connection():
+def get_db():
     try:
-        connection = mysql.connector.connect(
+        return mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             port=os.getenv("DB_PORT"),
             database=os.getenv("DB_NAME"),
+            ssl_verify_cert=False,
+            ssl_verify_identity=False
             ssl_ca="/etc/ssl/certs/ca-certificates.crt",
             ssl_disabled=False
         )
-        return connection
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
+    except Exception as e:
+        logger.error(f"DB Error: {e}")
+        raise e
